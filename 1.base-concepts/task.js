26f36ee29a1616b0eb8e1b1d24aed39ede9b806c
Anna Kuznetsova <ann_kuzn@mail.ru>
2024-01-21 21:28:07 +0500

    Решение двух задач

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,26 @@
-"use strict"
+"use strict";
 function solveEquation(a, b, c) {
   let arr = [];
-  
+  let d = b ** 2 - 4 * a * c;
+  if (d === 0) {
+    arr.push(-b / (2 * a));
+    return arr;
+  }
+
+  if (d > 0) {
+    arr.push((-b + Math.sqrt(d)) / (2 * a));
+    arr.push((-b - Math.sqrt(d)) / (2 * a));
+    return arr;
+  }
+
   return arr;
 }
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
+  let monthlyPercent = percent / 100 / 12;
+  let creditBody = amount - contribution;
+  let payment = creditBody * (monthlyPercent + (monthlyPercent / (((1 + monthlyPercent) ** countMonths) - 1)));
+  let totalMortgage = (payment * countMonths).toFixed(2);
+
+  return Number(totalMortgage);
 }
